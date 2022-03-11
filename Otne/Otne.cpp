bb97eabe9484ce5cd4cb96bd0f8ac16c3d5e09d3
beanflame <beanflame@qq.com>
2022-03-12 00:37:38 +0800

    Update Otne.cpp

--- Otne/Otne.cpp
+++ Otne/Otne.cpp
@@ -9,16 +9,30 @@
 
 using namespace std;
 
+void command_func_line(string str) {
+
+    wstring_convert<codecvt_utf8<i18nChar>> conv;
+    if (str == "-help" || str == "-?") {
+        wcout << OtneReadFile(L"help.txt");
+    }
+    else if(str == "-v" || str == "-version" || str == "-version")
+    {
+        wcout << L"Otne version 0.0.1" << endl;
+    }
+    else {
+        i18nString source = OtneReadFile(conv.from_bytes(str));
+        //wcout << source << endl;
+        //wcout << L"----------------------------------" << endl;
+        Htoken(source);
+    }
+}
+
 int main(int argc, char* argv[]) {
     std::setlocale(LC_ALL, "");  // MinGW gcc / MSVC
     std::ios_base::sync_with_stdio(false);  // Linux gcc
     std::ios::sync_with_stdio(false);    // Linux gcc
-
-    // wcout << "你好，世界" << endl;
-    
-    i18nString source = OtneReadFile(L"code.txt");
-    
-    wcout << source << endl;
-    wcout << L"----------------------------------" << endl;
-    Htoken(source);
+    for (int i = 0; i < argc ; i++) {
+        if (i != 0) { command_func_line(argv[i]); }
+    }
+    return 0;
 }
