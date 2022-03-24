39af6268f5bb1d06d249b6a36aefabc021c998f8
beanflame <beanflame@qq.com>
2022-03-24 10:12:29 +0800

    Update Otne.cpp

--- Otne/Otne.cpp
+++ Otne/Otne.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <codecvt>
-#include <windows.h>
-
 #include "OtneFile/OtneFile.h"
 #include "Otne_utf8.h"
 #include "lexer/lexer.h"
-
 using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL, "");          // Linux MinGW GCC MSVC
+    ios::sync_with_stdio(false);    // Linux gcc     
+    i18nString source = OtneReadFile("code.otne");
+    wcout << source << endl;
+    wcout << L"----------------------------------" << endl;
+    lexer(source);
+    return 0;
+}
+
+
+
+/*
+for (int i = 0; i < argc ; i++) {
+    if (i != 0) { command_func_line(argv[i]); }
+}
+*/
+
+/*
+
+func main () {
+}
+
+#include <stdio.h>
+
+int main()
+{
+    // 我的第一个 C 程序
+printf("Hello, World! \n");
+
+return 0;
+}
+*/
+
+
+
+
 
 /*
 void command_func_line(string str)
@@ -27,22 +64,3 @@ void command_func_line(string str)
     }
 }
 */
-int main(int argc, char *argv[])
-{
-    setlocale(LC_ALL, "");            // Linux MinGW GCC MSVC
-    ios::sync_with_stdio(false);      // Linux gcc
-
-    i18nString source = OtneReadFile("code.otne");
-    wcout << source << endl;
-    wcout << L"----------------------------------" << endl;
-    lexer(source);
-
-
-    /*
-    for (int i = 0; i < argc ; i++) {
-        if (i != 0) { command_func_line(argv[i]); }
-    }
-    */
-
-    return 0;
-}
